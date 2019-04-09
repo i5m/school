07107aa36944718a780b47ba3b558206df2fd766
Ishan Mathur <49003172+ishanmathur@users.noreply.github.com>
2019-04-09 15:53:52 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,38 +1,91 @@
 <?php require_once('header.php'); ?>
 
 <style> 
-    .btn {
-        width: 165px;
-        height: 75px;
-        border-top-left-radius: 25px;
-        border-top-right-radius: 25px;
-        border-bottom-left-radius: 25px;
-        margin-top: 10px;
-        margin-left: 7px;
-    }   
+    @media only screen and (max-width: 3400px) {
+        #indexbtn .btn {
+            width: 300px;
+            height: 150px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            margin-top: 20px;
+            margin-left: 14px;
+            }  
+    }
+    @media only screen and (max-width: 1260px) {
+        #indexbtn .btn {
+            width: 250px;
+            height: 125px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            margin-top: 20px;
+            margin-left: 14px;
+            }  
+    }
+    @media only screen and (max-width: 1100px) {
+        #indexbtn .btn {
+            width: 200px;
+            height: 100px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            margin-top: 10px;
+            margin-left: 7px;
+            }  
+    }
+    @media only screen and (max-width: 860px) {
+        #indexbtn .btn {
+            width: 160px;
+            height: 75px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            margin-top: 10px;
+            margin-left: 7px;
+            }  
+    }
+    @media only screen and (max-width: 580px) {
+        #indexbtn .btn {
+            width: 145px;
+            height: 75px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            margin-top: 10px;
+            margin-left: 7px;
+            }  
+    }
+    @media only screen and (max-width: 350px) {
+        #indexbtn .btn {
+            width: 135px;
+            height: 80px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            margin-top: 10px;
+            margin-left: 7px;
+            }  
+    }
 </style>
 
 </head>
     <body>
+        <?php require_once('navbar.php'); ?>
         <br><br>
         <div id="indexbtn" align="center">
-            <button type="button" style="width: 170px; height: 50px; border-radius: 15px; margin: 7px;" onclick="window.location.href='assignments.php'" class="btn btn-secondary">
-                <i class="material-icons">assignment</i> <b> Assignments </b>
-            </button>
-            <button type="button" style="width: 170px; height: 50px; border-radius: 15px; margin: 7px;" onclick="window.location.href='login.php'" class="btn btn-secondary">
-                <i class="material-icons">lock</i> <b> Log in </b>
-            </button><br>
-            <br>
+            <br><br>
             <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl1" class="btn btn-outline-primary"> <i class="material-icons">book</i> <b> Advanced Java </b> </button>
             <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl2" class="btn btn-outline-success"> <i class="material-icons">book</i> <b> Engineering Physics </b> </button>
             <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl3" class="btn btn-outline-warning"> <i class="material-icons">book</i> <b> Employblity Skills </b> </button>
             <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl4" class="btn btn-outline-danger"> <i class="material-icons">book</i> <b> Basics of MATLAB </b> </button><br>
-            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl5" class="btn btn-outline-primary"> <i class="material-icons">book</i> <b> Workshop Practice </b> </button>
-            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl6" class="btn btn-outline-success"> <i class="material-icons">book</i> <b> Engineering Mecahnics </b> </button>
-            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl7" class="btn btn-outline-warning"> <i class="material-icons">book</i> <b> Maths II </b> </button>
-            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl8" class="btn btn-outline-danger"> <i class="material-icons">book</i> <b> Prof Comm </b> </button><br><br>
-        </div>
+            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl5" class="btn btn-outline-success"> <i class="material-icons">book</i> <b> Workshop Practice </b> </button>
+            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl6" class="btn btn-outline-danger"> <i class="material-icons">book</i> <b> Engineering Mecahnics </b> </button>
+            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl7" class="btn btn-outline-primary"> <i class="material-icons">book</i> <b> Maths II </b> </button>
+            <button type="button" data-toggle="modal" data-target=".bd-example-modal-xl8" class="btn btn-outline-warning"> <i class="material-icons">book</i> <b> Prof Comm </b> </button><br><br>
+            <br><br><br>
+            <?php require_once('modalindex.php'); ?>
 
-        <?php require_once('modalindex.php'); ?>
+        </div>
 
 <?php require_once('footer.php'); ?>
--- welcome.php
+++ welcome.php
@@ -11,29 +11,37 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
  
 <?php require_once('header.php'); ?>
 <style>
-    #welcmpage {
-        position: absolute;
-        top: 55%;
-        left: 50%;
-        transform: translate(-50%,-50%);
+    @media only screen and (max-width: 3400px) {
+        #fixsize {
+            font-size: 35px;
+        }
+    }
+    @media only screen and (max-width: 600px) {
+        #fixsize {
+            font-size: 25px;
+        }
+    }
+    @media only screen and (max-width: 450px) {
+        #fixsize {
+            font-size: 18px;
+        }
     }
 </style>
 </head>
 <body>
     <?php require_once('navbar.php'); ?>
-
-    <div id="welcmpage" align="center">
-
+<br>
+    <div align="center" style="margin: 20px;">
+        <br><br>
         <div class="page-header">
-            <h2>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.<br><br>
-            This is<br> &copy; <b>Ishan Mathur</b> Production </h2><br>
+            <p id="fixsize">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.<br><br>
+            This is &copy; <b>Ishan Mathur</b> Production </p><br>
         </div>
-        
+        <br>
             <a href="reset-password.php" style="margin: 10px;" class="btn btn-outline-warning"><i class="material-icons">hourglass_empty</i><b> Reset Your Password </b></a>
             <a href="logout.php" style="margin: 10px;" class="btn btn-outline-danger"><i class="material-icons">input</i><b> Sign Out of Your Account </b></a><br>
-            <a href="help.php" style="margin: 20px; width: 250px; border-radius: 15px; font-size: 27px;" class="btn btn-success"><i class="material-icons">group_work</i><b> Contribute </b></a>
+            <a href="help.php" style="margin: 20px;" class="btn btn-outline-primary"><i class="material-icons">group_work</i><b> Contribute </b></a>
         
-
     </div>
 
 <?php require_once('footer.php'); ?>
