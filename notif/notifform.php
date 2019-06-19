e26eff18cf8bca5fd6673d0617a89aa188364662
Ishan Mathur <49003172+i5m@users.noreply.github.com>
2019-06-19 11:17:53 -0700

    Update notifform.php

--- notif/notifform.php
+++ notif/notifform.php
@@ -12,10 +12,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <?php require_once('../header.php'); ?>
 </head>
 <body>
-
     <form action="getnotif.php" method="post">
         <textarea rows="5" name="comment">sdwdsc</textarea>
         <input type="submit" value="submit">
     </form>
-
 <?php require_once('../footer.php'); ?>
